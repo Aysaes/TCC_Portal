c9c957d97540902d8ad8270c5b6e98da91ca4852
Kurdapya <Aysae@aol.com>
2026-03-25 17:08:29 +0800

    HR Admin Overview

--- resources/js/Config/navigation.jsx
+++ resources/js/Config/navigation.jsx
@@ -176,6 +176,11 @@ export const getHRAdminLinks = (auth) => {
             href: route('hr.admin.index'),
             active: route().current('hr.admin.index') 
         },
+        { 
+            label: 'HR Module', 
+            href: route('hr.index'),
+            active: route().current('hr.index') 
+        },
         { label: 'Feedback Form Submissions', 
             href: route('hr.feedback.index'),
             active: route().current('hr.feedback.index')
