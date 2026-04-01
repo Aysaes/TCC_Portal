45f51e146caa2855e41efdb760c31a8f1fe9802b
xxxDRIxxx <bangalandoam@students.national-u.edu.ph>
2026-04-01 10:44:51 +0800

    Filtering added

--- resources/js/Layouts/SidebarLayout.jsx
+++ resources/js/Layouts/SidebarLayout.jsx
@@ -214,6 +214,12 @@ export default function SidebarLayout({
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
         ),
+        'PO Generation': (
+        <svg className="h-4 w-4 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6M7 3h8l4 4v14a2 2 0 01-2 2H7a2 2 0 01-2-2V5a2 2 0 012-2z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 3v4h4" />
+        </svg>
+),
     };
 
     const renderSidebarIcon = (link) => {
