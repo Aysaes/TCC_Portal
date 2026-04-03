5b57d27c2415f47027c6891dfff9cff772855013
Kurdapya <Aysae@aol.com>
2026-04-03 17:54:08 +0800

    test ulet

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;test
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
